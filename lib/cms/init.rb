10590e686b28694ba30437c4920327fe96f3f96d
Juan Alvarez <alce@mac.com>
2010-04-15 19:46:46 -0500

    Remove i18n initializer from Cms.generator_paths

--- lib/cms/init.rb
+++ lib/cms/init.rb
@@ -4,7 +4,7 @@ module Cms
 
     define_method(:root) { __root__ }
 
-    ::SPEC = eval(File.read(__root__ + '/browsercmsi.gemspec'))
+    ::SPEC = eval(File.read(__root__ + '/browsercms.gemspec'))
 
     attr_accessor :attachment_file_permission
 
@@ -114,6 +114,4 @@ Cms.add_generator_paths(Cms.root,
   "public/site/**/*",   
   "public/stylesheets/cms/**/*",
   "public/images/cms/**/*",
-  "db/migrate/[0-9]*_*.rb",
-  "config/locales/bcms/**/*",
-  "config/initializers/i18n.rb")
+  "db/migrate/[0-9]*_*.rb")
